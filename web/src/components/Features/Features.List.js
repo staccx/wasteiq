ffeea22c10da56efe6f1dea8008a1a61fcbeb58c
maris <mariselnes@users.noreply.github.com>
2019-09-12 12:51:19 +0200

    remove card styling from features

--- web/src/components/Features/Features.List.js
+++ web/src/components/Features/Features.List.js
@@ -1,25 +1,8 @@
 import React from "react"
 import styled from "styled-components"
 import { SanityImage } from "@staccx/sanity"
-import {
-  List as ListBase,
-  Heading,
-  Paragraph,
-  Layout,
-  theming
-} from "@staccx/base"
+import { List, Heading, Paragraph, Layout, theming } from "@staccx/base"
 import { i18nInstance } from "@staccx/i18n"
-
-const List = styled(ListBase)`
-  img {
-    width: 100%;
-  }
-  @media only screen and (min-width: ${theming.wrapper("medium")}) {
-    li {
-      flex-basis: calc(33% - 24px);
-    }
-  }
-`
 
 const t = val => i18nInstance.convert(val)
 const FeaturesList = ({ features }) => {
@@ -42,10 +25,7 @@ const FeaturesList = ({ features }) => {
 }
 
 const FeaturesCard = styled.div`
-  margin: 0 6.7%;
-  padding: 24px;
-  background: #ffffff;
-  box-shadow: 0px 0px 30px rgba(254, 121, 77, 0.1);
+  padding: 40px;
 `
 
 export default FeaturesList
