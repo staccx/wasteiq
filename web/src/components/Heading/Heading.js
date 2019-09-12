a64eb276e090452b389f05be1bf2828612cb14b9
Nils Norman Haukås <mail@nilsnh.no>
2019-09-12 11:07:38 +0200

    setup partners block, add Heading component

---
+++ web/src/components/Heading/Heading.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+import { Heading as HeadingBase } from "@staccx/base"
+
+const Heading = styled(HeadingBase)`
+  font-weight: bold;
+`
+
+export default Heading
--- web/src/components/Partners/Partners.js
+++ web/src/components/Partners/Partners.js
@@ -1,38 +1,48 @@
 import React from "react"
 
-import { Layout, Heading, Paragraph, Divider } from "@staccx/base"
+import { Layout, Paragraph, Divider } from "@staccx/base"
 import { SanityQuery, SanityImage } from "@staccx/sanity"
 import styled from "styled-components"
 import ButtonGroup from "../ButtonGroup/ButtonGroup"
+import Heading from "../Heading/Heading"
 
 const ImageContainer = styled.div`
+  text-align: center;
+`
+
+const PartnersWrapper = styled.div`
   text-align: center;
 `
 
 const Partners = ({ heading, lede, partners = [], buttons }) => {
   return (
     <Layout as={"article"}>
-      <header>
-        <Layout rowGap={"small"}>
-          {heading && (
-            <Heading variant="lined" level={2}>
-              {heading}
-            </Heading>
-          )}
-          {lede && <Paragraph>{lede}</Paragraph>}
-          {buttons && <ButtonGroup buttons={buttons} />}
-          {partners && partners.length > 0 && <Divider />}
-        </Layout>
-        <SanityQuery id={"partners-block"} query={buildPartnerQuery(partners)}>
-          {({ result = [] }) => {
-            return result.map(({ _id, partnerLogo }) => (
-              <ImageContainer key={_id}>
-                <SanityImage image={partnerLogo} />
-              </ImageContainer>
-            ))
-          }}
-        </SanityQuery>
-      </header>
+      <PartnersWrapper>
+        <header>
+          <Layout rowGap={"small"}>
+            {heading && (
+              <Heading variant="lined" level={2}>
+                {heading}
+              </Heading>
+            )}
+            {lede && <Paragraph>{lede}</Paragraph>}
+            {buttons && <ButtonGroup buttons={buttons} />}
+            {partners && partners.length > 0 && <Divider />}
+          </Layout>
+          <SanityQuery
+            id={"partners-block"}
+            query={buildPartnerQuery(partners)}
+          >
+            {({ result = [] }) => {
+              return result.map(({ _id, partnerLogo }) => (
+                <ImageContainer key={_id}>
+                  <SanityImage image={partnerLogo} />
+                </ImageContainer>
+              ))
+            }}
+          </SanityQuery>
+        </header>
+      </PartnersWrapper>
     </Layout>
   )
 }
