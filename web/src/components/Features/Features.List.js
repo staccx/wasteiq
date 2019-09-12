16c461d797e3a3e4b4a586da6649b4364c03b077
maris <mariselnes@users.noreply.github.com>
2019-09-12 12:34:23 +0200

    Style Features List

--- web/src/components/Features/Features.List.js
+++ web/src/components/Features/Features.List.js
@@ -1,7 +1,25 @@
 import React from "react"
+import styled from "styled-components"
 import { SanityImage } from "@staccx/sanity"
-import { List, Heading, Paragraph, Layout } from "@staccx/base"
+import {
+  List as ListBase,
+  Heading,
+  Paragraph,
+  Layout,
+  theming
+} from "@staccx/base"
 import { i18nInstance } from "@staccx/i18n"
+
+const List = styled(ListBase)`
+  img {
+    width: 100%;
+  }
+  @media only screen and (min-width: ${theming.wrapper("medium")}) {
+    li {
+      flex-basis: calc(33% - 24px);
+    }
+  }
+`
 
 const t = val => i18nInstance.convert(val)
 const FeaturesList = ({ features }) => {
@@ -9,15 +27,32 @@ const FeaturesList = ({ features }) => {
     <List variant={"columns"}>
       {features.map(feature => (
         <li key={feature._key}>
-          <Layout rowGap={"tiny"}>
+          <Layout rowGap={"none"}>
             {feature.image && <SanityImage image={feature.image} />}
-            {feature.title && <Heading level={4}>{t(feature.title)}</Heading>}
-            {feature.body && <Paragraph>{t(feature.body)}</Paragraph>}
+
+            <FeaturesCard>
+              {feature.title && <Heading level={4}>{t(feature.title)}</Heading>}
+              {feature.body && <Paragraph>{t(feature.body)}</Paragraph>}
+            </FeaturesCard>
           </Layout>
         </li>
       ))}
     </List>
   )
 }
+
+const FeaturesCard = styled.div`
+  background: red;
+  margin: 0 6.7%;
+  padding: 24px;
+  background: #ffffff;
+  /* orange */
+  box-shadow: 0px 0px 30px rgba(254, 121, 77, 0.1);
+`
+const FeaturesImage = styled.div`
+  img {
+    width: 100%;
+  }
+`
 
 export default FeaturesList
