d3dd4bb63585e1237a703035fdee3a527b79ff20
Nils Norman Haukås <mail@nilsnh.no>
2019-09-12 12:32:15 +0200

    use new Heading and Lede components for Features

--- web/src/components/Features/Features.js
+++ web/src/components/Features/Features.js
@@ -1,24 +1,26 @@
 import React from "react"
 
-import { Layout, Heading, Paragraph, Divider } from "@staccx/base"
+import { Layout } from "@staccx/base"
+import styled from "styled-components"
 import ButtonGroup from "../ButtonGroup/ButtonGroup"
+import Heading from "../Heading/Heading"
+import Lede from "../Lede/Lede"
 import FeaturesList from "./Features.List"
+
+const Header = styled.header`
+  text-align: center;
+`
 
 const Features = ({ heading, lede, features, buttons }) => {
   return (
     <Layout as={"article"}>
-      <header>
+      <Header>
         <Layout rowGap={"small"}>
-          {heading && (
-            <Heading variant="lined" level={2}>
-              {heading}
-            </Heading>
-          )}
-          {lede && <Paragraph>{lede}</Paragraph>}
+          {heading && <Heading level={2}>{heading}</Heading>}
+          {lede && <Lede>{lede}</Lede>}
           {buttons && <ButtonGroup buttons={buttons} />}
-          {features && features.features && <Divider />}
         </Layout>
-      </header>
+      </Header>
       {features &&
         features.features && <FeaturesList features={features.features} />}
     </Layout>
